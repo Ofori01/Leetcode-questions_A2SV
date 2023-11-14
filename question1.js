8178a1615cade2c64c489e358ebe664b30676800
Amoah Ofori Darkwah <oforidarkwah7@gmail.com>
2023-11-14 11:56:04 +0000

    final answer. Second code is more efficient and uses a little less memory

--- question1.js
+++ question1.js
@@ -3,12 +3,29 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    Array.form(n,
-        (i) =>{
-            return i%5 === 0 && i%5 === 0 ? "FizzBuzz":
+   return Array.from({length: n},
+        (n,i) =>{
+            i+=1;
+            return (i%5 === 0 && i%3 === 0 ? "FizzBuzz":
             i%3 ===0 ? "Fizz":
-            i%5 === 0 ? "Buzz": i;
+            i%5 === 0 ? "Buzz": `${i}`)
         }
         )
+    
+};
+/*
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let res =[];
+    for (i=1; i < n+1; ++i){
+        (i % 5 ===0 && i %3 ===0 ? res.push('FizzBuzz'): i % 3===0 ? res.push('Fizz'): i % 5===0 ? res.push('Buzz'): res.push(`${i}`));
+
+
+    }
+   
+        return res
+        
     
 };
